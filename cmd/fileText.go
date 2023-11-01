b57dc5708d79f0997b3d2c47399d0350a5389310
antonag32 <antonio@vauxoo.com>
2023-10-31 19:30:41 -0600

    fix: don't parse flags in init
    This is bad practice which results in spaghetti code down the line.
    Moving flag parsing to the actual command execution is cleaner.
    
    See: https://github.com/spf13/cobra/issues/553

--- cmd/fileText.go
+++ cmd/fileText.go
@@ -8,28 +8,23 @@ import (
 )
 
 var fileTextCmd = &cobra.Command{
-	Use:   "file-text name text",
-	Short: "Search for text inside a file across repositories",
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 2 {
-			return fmt.Errorf("2 arguments are required but %d were provided", len(args))
-		}
-		return nil
-	},
-	Run: fileTextExecute,
+	Use:                "file-text name text",
+	Short:              "Search for text inside a file across repositories",
+	Args:               cobra.ExactArgs(2),
+	Run:                fileTextExecute,
+	DisableFlagParsing: true,
 }
 
-func init() {
-	fileTextCmd.Flags().CountVarP(&verbose, "verbose", "v", "Make the operation more talkative")
-	search = fileTextCmd.Flags().String("search", "", "Search criteria")
-}
+func fileTextExecute(cmd *cobra.Command, args []string) {
+	var verbose = cmd.Flags().CountP("verbose", "v", "Make the operation more talkative")
+	var search = cmd.Flags().String("search", "", "Search criteria")
+	cobra.CheckErr(cmd.Flags().Parse(args))
 
-func fileTextExecute(_ *cobra.Command, args []string) {
 	ch := make(chan ProjectFile)
 	fileName := args[0]
 	searchedText := args[1]
 
-	go fileSearch(fileName, search, []string{"12.0", "13.0", "14.0", "15.0", "16.0"}, verbose, ch)
+	go fileSearch(fileName, search, []string{"12.0", "13.0", "14.0", "15.0", "16.0"}, *verbose, ch)
 	for pj := range ch {
 		content, err := base64.StdEncoding.DecodeString(pj.file.Content)
 		if err != nil {
